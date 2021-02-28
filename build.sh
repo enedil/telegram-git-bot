520bb039c4823f6c398a73b8b0105caeb3824972
janczarknurek <jacek@beit.tech>
2021-02-28 22:57:00 +0100

    Podman is now a valid container engine
    Signed-off-by: janczarknurek <jacek@beit.tech>

--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 
 repo_root="$(dirname "$(realpath $0)")"
 
+if command -v docker &> /dev/null; then
+    DOCKER=docker
+elif command -v podman &> /dev/null; then
+    DOCKER=podman
+else
+    echo "Install docker or something idk"
+    exit -1
+fi
+
 cd $repo_root
 ./prepare.sh
-docker build -t tel_git_bot_builder .
-docker run --volume "$repo_root:/sourcetree:Z" \
+$DOCKER build -t tel_git_bot_builder .
+$DOCKER run --volume "$repo_root:/sourcetree:Z" \
         tel_git_bot_builder
