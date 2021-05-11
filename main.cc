cd684a7813a42d32837b37d457e12e78125e7e97
janczarknurek <janczar.kurek@student.uj.edu.pl>
2021-05-11 23:05:00 +0200

    Still dont working
    Signed-off-by: janczarknurek <janczar.kurek@student.uj.edu.pl>

--- main.cc
+++ main.cc
@@ -1,21 +1,20 @@
 #include <seastar/core/app-template.hh>
 #include <seastar/core/future.hh>
+#include <seastar/core/thread.hh>
 #include <seastar/http/httpd.hh>
 
-/* int main(int argc, char** argv) {
-    seastar::app_template app;
-    app.run(argc, argv, [] {
-           return seastar::make_ready_future<>(); 
-    });
-}*/
 using namespace seastar;
-int main() {
-	httpd::http_server_control my_server;
-	my_server.start("my-http-server").then([&my_server] {
-    		return my_server.set_routes([](routes & r) {
-	});
-	}).then([&my_server] {
-	return my_server.listen(ipv4_addr{"127.0.0.1", 2137}).then([] {
+
+future<> run_server() {
+  auto * my_server = new httpd::http_server_control;
+  return my_server->start("my-http-server").then([&my_server] {
+        return my_server->set_routes([](routes & r) {});
+  }).then([&my_server] {
+        return my_server->listen(ipv4_addr{"127.0.0.1", 2137});
     });
-});
+}
+
+int main(int ac, char** av) {
+    seastar::app_template app;
+    app.run(ac, av, run_server);
 }
