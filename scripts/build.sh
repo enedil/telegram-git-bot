e5656220a600931ec7dba450a482c7396245b634
Michał Radwański <enedil@outlook.com>
2021-05-12 03:00:34 +0200

    CMakeLists: remove C++20 (causes build issues), quote path in scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sourcetree=$1
+sourcetree=$(readlink -f "$1")
 
 cd $sourcetree/external/seastar
 ./configure.py --mode=release --without-apps --without-tests --without-demos
@@ -8,5 +8,5 @@ ninja-build -C build/release/
 
 echo $sourcetree/external/seastar/
 cd ../../build
-cmake ..  -DSEASTAR_DIR=$sourcetree/external/seastar/ -GNinja
+cmake ..  -DSEASTAR_DIR="$sourcetree/external/seastar/" -GNinja
 ninja
